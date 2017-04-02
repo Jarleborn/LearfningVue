4f756dab5da04ed93fdb858d25865c9ba50ada26
hj222hi <hj222hi@student.lnu.se>
2017-04-02 03:00:10 +0200

    done I think

--- component.js
+++ component.js
@@ -3,51 +3,15 @@ Vue.component('todo-item', {
   template: '<li>{{todo.text}} </li>'
 })
 
-
-const app7 = new Vue({
-  el: '.app-7',
-  data: {
-    list:[
-
-        {text: 'Numel'},
-        {text: 'Gulpin'},
-        {text: 'Tailow'},
-        {text: 'Beware'},
-
-    ]
-  }
+Vue.component('my-component', {
+  template: '<div> A custom county chooper <div>'
 })
-
-const vm = new Vue({
-  data: {
-    a: 1
+const Child = {
+  template: '<div>A custom component!</div>'
+}
+new Vue({
+  el: '#exampleCOmp',
+  components:{
+    'my-component': Child
   },
-  created: function () {
-    // `this` points to the vm instance
-    console.log('a is: ' + this.a)
-  }
-})
-
-const buts = new Vue({
-  el: '#but',
-  data: {
-    someDynamicCondition: false,
-  },
-
-  methods: {
-    func: function(){
-      console.log('tjoflöjt')
-      this.someDynamicCondition = true
-    }
-  }
-})
-
-
-
-const aS = new Vue({
-  el: '.aTag',
-  data: {
-    url: 'http://jarleborn.com',
-  },
-
 })
--- script.js
+++ script.js
@@ -147,3 +147,50 @@ const bForm = new Vue({
     message: '',
   }
 })
+
+const vm = new Vue({
+  data: {
+    a: 1
+  },
+  created: function () {
+    // `this` points to the vm instance
+    console.log('a is: ' + this.a)
+  }
+})
+
+const buts = new Vue({
+  el: '#but',
+  data: {
+    someDynamicCondition: false,
+  },
+
+  methods: {
+    func: function(){
+      console.log('tjoflöjt')
+      this.someDynamicCondition = true
+    }
+  }
+})
+
+
+
+const aS = new Vue({
+  el: '.aTag',
+  data: {
+    url: 'http://jarleborn.com',
+  },
+
+})
+
+const app7 = new Vue({
+  el: '.app-7',
+  data: {
+    list:[
+
+        {text: 'Numel'},
+        {text: 'Gulpin'},
+        {text: 'Tailow'},
+        {text: 'Beware'},
+    ]
+  }
+})
